d23a5df82d5a3dc3b943caecfd74bc0e58cc5117
Gérard Dethier <info@gerarddethier.be>
2024-07-23 14:53:02 +0200

    chore: handle deps upgrades.

--- copy_jar.sh
+++ copy_jar.sh
@@ -2,20 +2,20 @@
 
 # Common
 rm *.jar
-cp ~/.m2/repository/org/slf4j/slf4j-api/1.7.36/slf4j-api-1.7.36.jar .
-cp ~/.m2/repository/org/slf4j/slf4j-simple/1.7.30/slf4j-simple-1.7.30.jar .
+cp ~/.m2/repository/org/slf4j/slf4j-api/2.0.13/slf4j-api-2.0.13.jar .
+cp ~/.m2/repository/org/slf4j/slf4j-simple/2.0.13/slf4j-simple-2.0.13.jar .
 
 # Doc
 cp ../pousse-cafe-doc/target/*.jar .
-cp ~/.m2/repository/org/freemarker/freemarker/2.3.30/freemarker-2.3.30.jar .
-cp ~/.m2/repository/org/xhtmlrenderer/flying-saucer-core/9.1.20/flying-saucer-core-9.1.20.jar .
-cp ~/.m2/repository/org/xhtmlrenderer/flying-saucer-pdf/9.1.20/flying-saucer-pdf-9.1.20.jar .
-cp ~/.m2/repository/com/lowagie/itext/2.1.7/itext-2.1.7.jar .
-cp ~/.m2/repository/commons-io/commons-io/2.7/commons-io-2.7.jar .
+cp ~/.m2/repository/org/freemarker/freemarker/2.3.33/freemarker-2.3.33.jar .
+cp ~/.m2/repository/org/xhtmlrenderer/flying-saucer-core/9.9.0/flying-saucer-core-9.9.0.jar .
+cp ~/.m2/repository/org/xhtmlrenderer/flying-saucer-pdf/9.9.0/flying-saucer-pdf-9.9.0.jar .
+cp ~/.m2/repository/com/github/librepdf/openpdf/2.0.2/openpdf-2.0.2.jar .
+cp ~/.m2/repository/commons-io/commons-io/2.16.1/commons-io-2.16.1.jar .
 
 # Source
 cp ../pousse-cafe-source/target/*.jar .
-cp ~/.m2/repository/org/antlr/antlr4-runtime/4.8-1/antlr4-runtime-4.8-1.jar .
+cp ~/.m2/repository/org/antlr/antlr4-runtime/4.13.1/antlr4-runtime-4.13.1.jar .
 
 # Core
 cp ../pousse-cafe/pousse-cafe-core/target/*.jar .
